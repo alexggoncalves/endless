2517603905995182aad8ca81db9c706423ee3595
gonbar4 <goncalo.barroso@netcabo.pt>
2024-01-07 16:18:02 +0000

    Html part of the song details

--- src/components/Song/Song.jsx
+++ src/components/Song/Song.jsx
@@ -18,22 +18,73 @@ function Song() {
 
     if (song) {
         return (
-            <div className="song-details-container">
-                <Link
-                    to={`${
-                        location.pathname?.includes("archive")
-                            ? "/endless/archive"
-                            : "/endless/"
-                    }`}
-                >
-                    bacc
-                </Link>
-                <h1>{song.title}</h1>
-                <img
-                    height={400}
-                    src={song.metadata.cover_image.imgix_url}
-                    alt={song.title + " cover art"}
-                />
+            <div className="song-details-container">          
+                <div>
+                    <Link
+                        to={`${
+                            location.pathname?.includes("archive")
+                                ? "/endless/archive"
+                                : "/endless/"
+                        }`}
+                    >
+                        back
+                    </Link>
+                </div>
+                <div className="song-details-info-container">
+                    <div>
+                        <img
+                            height={400}
+                            src={song.metadata.cover_image.imgix_url}
+                            alt={song.title + " cover art"}
+                        />
+                    </div>
+                    <div className="song-details-info">
+                        <div>
+                            <h1>{song.title}</h1>
+                            <h2>by {song.metadata.artist[0].title}</h2>
+                            <Link
+                                to={`${
+                                    location.pathname?.includes("archive")
+                                        ? "/endless/archive"
+                                        : "/endless/"
+                                }`}
+                            >
+                                Listen on spotify
+                            </Link>
+                        </div>
+                        <div>
+                            <h3>GENRE</h3>
+                            <h4>{song.metadata.genre}</h4>
+                            <h3>RELEASE YEAR</h3>
+                            <h4>{song.metadata.year}</h4>
+                            <h3>ALBUM</h3>
+                            <h4>{song.metadata.album.title}</h4>
+                            <h3>DURATION</h3>
+                            <h4>{song.metadata.duration}</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="song-details-extra-info-container">
+                    <div>
+                        <h2>THE ALBUM</h2>
+                        <h1>{song.metadata.album.title}</h1>
+                        <ol>
+                            {song.metadata.album.metadata.track_list.split('\n').map((track, index) => (
+                                <li key={index}>{track}</li>
+                            ))}
+                        </ol>
+                    </div>   
+                    <div>
+                        <h2>THE ARTIST</h2>
+                        <h1>{song.metadata.artist[0].title}</h1>
+                        <p>{song.metadata.artist[0].metadata.biography}</p>
+                        <img
+                            height={400}
+                            src={song.metadata.artist[0].metadata.photo.url}
+                            alt={song.metadata.artist[0].title + " artist photo"}
+                        />                     
+                    </div>                      
+                </div>
             </div>
         );
     } else return <></>;
