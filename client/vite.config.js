9d086b4c09ecf45675e2d6ff88a1b890f99e7373
alexggoncalves <alex.ggoncalves@hotmail.com>
2025-08-05 22:49:40 +0100

    minor fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['2f543ba53046.ngrok-free.app']
-  }
+  plugins: [react()]
 })
